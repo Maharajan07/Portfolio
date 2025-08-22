9486cc6796c99a119ac4cbac96928c18d09b3f18
Maharajan07 <maharajan03007@gmail.com>
2025-08-22 12:15:14 +0530

    helper fix

--- app/components/helper/animation-lottie.jsx
+++ app/components/helper/animation-lottie.jsx
@@ -1,19 +1,18 @@
 "use client";
 
-import Lottie from "lottie-react";
+import dynamic from "next/dynamic";
+
+// Dynamically import Lottie to prevent SSR errors
+const Lottie = dynamic(() => import("lottie-react"), { ssr: false });
 
 const AnimationLottie = ({ animationPath, width }) => {
-  const defaultOptions = {
-    loop: true,
-    autoplay: true,
-    animationData: animationPath,
-    style: {
-      width: '95%',
-    }
-  };
-
   return (
-    <Lottie {...defaultOptions} />
+    <Lottie
+      animationData={animationPath}
+      loop
+      autoplay
+      style={{ width: width || "95%" }}
+    />
   );
 };
 
