1e94bc22ae8bbead36c6cf1928a9af650af8e3a9
Maharajan07 <maharajan03007@gmail.com>
2025-08-22 13:10:32 +0530

    Final Commit

--- app/layout.js
+++ app/layout.js
@@ -12,6 +12,9 @@ export const metadata = {
   title: "Portfolio of Maharajan - Software Developer",
   description:
     "This is the portfolio of Maharajan. I am a full stack developer and a self taught developer. I love to learn new things and I am always open to collaborating with others. I am a quick learner and I am always looking for new challenges.",
+    icons: {
+    icon: "/favicon.ico",
+  },
 };
 
 export default function RootLayout({ children }) {
