ce845344cbdc15ecc667ff48044cdee128088b70
Maharajan07 <maharajan03007@gmail.com>
2025-08-22 13:00:04 +0530

    resume added

--- utils/data/experience.js
+++ utils/data/experience.js
@@ -4,17 +4,5 @@ export const experiences = [
     title: 'Java Full-Stack Developer Intern',
     company: "Besant Technologies",
     duration: "(June 2024 - Present)"}
-  // },
-  // {
-  //   id: 2,
-  //   title: "FullStack Developer",
-  //   company: "Fiverr (freelance)",
-  //   duration: "(Jun 2021 - Jan 2022)"
-  // },
-  // {
-  //   id: 3,
-  //   title: "Self Employed",
-  //   company: "Code and build something in everyday.",
-  //   duration: "(Jan 2018 - Present)"
-  // }
+
 ]
