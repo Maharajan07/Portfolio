db8086c941a14b9029096306751696b1b51799f2
Maharajan07 <maharajan03007@gmail.com>
2025-08-22 12:27:01 +0530

    profile fix

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -1,6 +1,6 @@
 export const personalData = {
   name: "Maharajan",
-  profile: '/profile1.jpg',
+  profile: '/Profile1.jpg',
   designation: "Software Developer",
   description: "My name is Maharajan. I am a professional and enthusiastic programmer in my daily life. I am a quick learner with a self-learning attitude. I love to learn and explore new technologies and am passionate about problem-solving. I love almost all the stacks of web application development and love to make the web more open to the world. My core skill is based on JavaScript and I love to do most of the things using JavaScript. I am available for any kind of job opportunity that suits my skills and interests.",
   email: 'maharajan03007@gmail.com',
